fa404fb312c3d11cc6020a2b37f5f913ec9bc7d8
unknown <ian.albuquerque.silva@gmail.com>
2017-11-25 23:23:54 -0200

    squares movement

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
+import { setInterval } from 'timers';
 
 const app = express();
 
@@ -10,21 +11,34 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
+let id = 0;
+const positions: { x: number, y: number }[] = [];
+
 wss.on('connection', (ws: WebSocket) => {
+  const my_id = id;
+  id += 1;
+  positions.push({ x: 100, y: 100 });
 
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
+  //connection is up, let's add a simple simple event
+  ws.on('message', (message: string) => {
+    console.log(`IN from ${my_id} <`, message);
+    let position: {x: number, y: number} = JSON.parse(message);
+    positions[my_id] = position;
+    // ws.send(`Hello, you sent -> ${message}`);
+  });
 
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+  //send immediatly a feedback to the incoming connection
+  ws.send(JSON.stringify({id: my_id}));
 });
+
+setInterval(() => {
+  wss.clients
+  .forEach((client: WebSocket) => {
+    client.send(JSON.stringify(positions));
+  });
+}, 33)
 
 //start our server
 server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port ${server.address().port} :)`);
+  console.log(`Server started on port ${server.address().port} :)`);
 });
